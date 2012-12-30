bb81a141802d991897188e338b09a0c260492ac4
roger.castaldo@gmail.com <roger.castaldo@gmail.com@9dd127d7-1813-66cf-bd34-d04258c76d7a>
2012-12-30 18:18:29 +0000

    importing in existing code base from other project to make an open source version of the implemented tftp server.

---
+++ Delegates.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace Org.Reddragonit.Net.TFTP
+{
+    public delegate void delLogLine(LogLevels level, string line);
+    public delegate Stream delGetStream(string filename,IPEndPoint endpoint);
+}
---
+++ ITFTPFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace Org.Reddragonit.Net.TFTP
+{
+    public interface ITFTPFile
+    {
+        bool CanProduceStream(IPEndPoint endPoint);
+        Stream GetStream(IPEndPoint endPoint);
+        string Path
+        {
+            get;
+        }
+    }
+}
